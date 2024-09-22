8e072eee7223aa32bd5d098b407e1d1a25e8f16a
MRiDuL-ICE <abdulwahab22400@gmail.com>
2024-09-23 01:02:16 +0600

    toggle button done

--- toggle.js
+++ toggle.js
@@ -1,3 +1,12 @@
 document.getElementById("donation-field").addEventListener('click', function(){
+    document.getElementById('history-section').classList.add('hidden');
+    document.getElementById('history-field').classList.remove('bg-[#B4F461]');
+    document.getElementById('donation-field').classList.add('bg-[#B4F461]');
     document.getElementById('donation-section').classList.remove('hidden');
+})
+document.getElementById("history-field").addEventListener('click', function(){
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('history-section').classList.remove('hidden');
+    document.getElementById('donation-field').classList.remove('bg-[#B4F461]');
+    document.getElementById('history-field').classList.add('bg-[#B4F461]');
 })
